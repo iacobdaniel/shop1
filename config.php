c549bd1fb2b0222377a9bcde65366ac27873e2f2
iacobdaniel <iacobdaniel17@gmail.com>
2017-07-12 11:22:34 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,1 @@
-<?php
 
-define('DB_SERVER',"localhost");
-define('DB_USERNAME',"root");
-define('DB_USER_PASS',"5points");
-define('DB_NAME', "shop1db");
-
-define('ADMIN_NAME', "admin");
-define('ADMIN_PASS', 'parola123');
-
-?>
